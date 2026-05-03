0a822a1866a136bd27203c2461d2df5487b0be5a
Alex Hochheiden <ahochheiden@mozilla.com>
2026-05-03 01:22:49 -0700

    Bug ### - try adding more job slots since there are sleeping cargo jobs most of the build r?#build
    5 sleeping at peak, ninja recommends +2 over core count, so +7

--- python/mozbuild/mozbuild/backend/ninja.py
+++ python/mozbuild/mozbuild/backend/ninja.py
@@ -54,6 +54,7 @@ from mozbuild.frontend.data import (
     VariablePassthru,
     WasmSources,
 )
+from mozbuild.util import cpu_count
 
 
 def _strip_tests(path):
@@ -625,6 +626,8 @@ class NinjaBackend(CommonBackend):
         output.start_progress()
 
         cmd = [ninja, "-C", config.topobjdir, "--jobserver-pool"]
+        if jobs == 0:
+            jobs = (cpu_count() or 1) + 7
         if jobs:
             cmd += ["-j", str(jobs)]
         if verbose:
